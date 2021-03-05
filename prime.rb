b1cd6935a9b36fa81d8c9f9e15c09a6c5babbb15
Mariah Morales <mjustine622@gmail.com>
2021-03-04 21:14:07 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+    i = 2
+    while i < number
+        return false if number % i == 0
+        i += 1
+    end 
+    number > 1 ? true : false
+end 
+        
+
